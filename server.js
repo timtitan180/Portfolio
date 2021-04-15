b7a932abc4dede425ef49f221f770a3cb3fbd910
Timothy Tudosa <timtudosa6@gmail.com>
2021-04-15 17:44:35 -0500

    Added database to save forms that interested employers send

--- server.js
+++ server.js
@@ -1,24 +1,63 @@
 const express = require('express');
 
+const mongoose = require("mongoose");
+
 const fs = require('fs');
 
 const path = require('path');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.json());
 
 const router = express.Router();
 
 app.use(express.static(path.join(__dirname,"public")));
 
+mongoose.connect("mongodb://localhost:27017/portfolioViewerdb",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{console.log("MONGOODB IS connected and ready to save data...")}).catch((err)=>{console.log(err)});
+
 const port = 4541;
+
+const PortfolioViewerSchema = new mongoose.Schema({
+  email:{type:String},companyName:{type:String},message:{type:String}
+});
+
+const PortfolioViewer = mongoose.model('portfolioviewer',PortfolioViewerSchema); 
 
 router.get('/',(req,res)=>{
   res.sendFile(fs.readFileSync("index.html"));
 });
 
 router.get('/contactpage',(req,res)=>{
-  res.sendFile(fs.readFileSync("generic.html"));
+
+  res.sendFile(fs.readFileSync("elements.html"));
 });
+
+app.post('/contactpage',(req,res)=>{
+   var email = req.body.email;
+   var companyName = req.body.companyName;
+   var message = req.body.message;
+
+   if(email.length > 0 || companyName.length > 0 || message.length > 0) {
+      inputsNotEntered = false;
+       console.log(email);
+       console.log(companyName);
+       console.log(message);
+       newPortfolioViewer = new PortfolioViewer({email:email,companyName:companyName,message:message},()=>{
+             newPortfolioViewer.save().then(()=>{console.log(newPortfolioViewer)});
+             res.redirect("/");
+       });
+     }
+     else {
+       res.redirect('/contactpage');
+       console.log("Could not get form");
+     }
+     res.sendFile("C:/Users/timtu/Desktop/MyPortfolio/public/elements.html");
+ });
 
 router.get('/elements',(req,res)=>{
   res.sendFile(fs.readFileSync("elements.html"));
