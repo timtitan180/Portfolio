08b3c6eeff36ac1f77f3437ff69232ca60c48d6f
Tim Tudosa <timtudosa6@gmail.com>
2021-12-18 17:16:24 -0600

    Added mysql to additional skills and started resolivng nodemailer issue again

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const mongoose = require("mongoose");
 
+const request = require("request");
+
 const nodemailer = require('nodemailer');
 
 
 require('dotenv').config();
 
 
-const portfolioViewer = require('./model/portfolioViewer.js');
+// const portfolioViewer = require('./model/portfolioViewer.js');
 
 const fs = require('fs');
 
@@ -21,8 +23,6 @@ app.use(express.urlencoded({extended:false}));
 
 app.use(express.json());
 
-const router = express.Router();
-
 app.use(express.static(path.join(__dirname,"public")));
 
 mongoose.connect("mongodb://localhost:27017/portfolioViewerdb",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{console.log("MONGOODB IS connected and ready to save data...")}).catch((err)=>{console.log(err)});
@@ -30,66 +30,47 @@ mongoose.connect("mongodb://localhost:27017/portfolioViewerdb",{useNewUrlParser:
 const port = 4541;
 
 
-router.get('/',(req,res)=>{
+app.get('/',(req,res)=>{
+  console.log("Website is live!");
   
-  res.sendFile(fs.readFile("index.html"));
-});
-
-router.get('/contactpage',(req,res)=>{
-  res.sendFile(fs.readFile("index.html"));
-});
-
-app.post('/contact_page',(req,res)=>{
-
-  var email = req.body.email;
-   var companyName = req.body.companyName;
-   var message = req.body.message;
-
-
   var transporter = nodemailer.createTransport({
-    service: 'yahoo',
-    auth: {
-      user: 'timtudosa6@gmail.com',
-      pass: process.env.password
-    }
-  });
-
-  var mailOptions = {
-    from:email,
-    to:"timtudosa6@gmail.com",
-    text:message
+  service: 'gmail',
+  auth: {
+    user: 'timtudosa6@gmail.com',
+    pass: 'Snake150!'
   }
-  
-  transporter.sendMail(mailOptions,(err)=>{
-    if(err) {
-      console.log("There was an error submitting the form.");
-    }
-  });
+});
 
-   if(email.length > 0 || companyName.length > 0 || message.length > 0) {
-      inputsNotEntered = false;
-       console.log(email);
-       console.log(companyName);
-       console.log(message);
-      //  portfolioViewer({email:email,companyName:companyName,message:message},()=>{
-      //        portfolioViewer.save().then(()=>{console.log(portfolioViewer)}).catch((err)=>{
-      //          console.log("Could not retrieve added data");
-      //          console.log(err);
-      //        });
-      //        console.log(portfolioViewer);
-      //        res.redirect("/contactpage");
-      //  });
-     }
-     else {
-       res.redirect('/');
-       console.log("Could not get form");
-     }
-  
-     
-     res.sendFile(fs.readFile("index.html"));
- });
+var mailOptions = {
+  from: 'timtudosa6@gmail.com',
+  to: 'timtudosa6@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
 
-router.get('/elements',(req,res)=>{
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+
+  res.send("index.html");
+
+});
+});
+
+app.get('/contactpage',(req,res)=>{
+  res.sendFile(fs.readFile("index.html"));
+});
+
+app.post('/',(req,res)=>{
+  console.log(req.body.email);
+  console.log(req.body.message);
+});
+
+
+app.get('/elements',(req,res)=>{
   res.sendFile(fs.readFile("index.html"));
 });
 
@@ -97,10 +78,31 @@ router.get('/elements',(req,res)=>{
 app.listen(port,(req,res)=>{
   try {
     console.log(`Server listening on port ${port}`);
-  }
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'timtudosa6@gmail.com',
+        pass: 'Snakes150!!'
+      }
+    });
+    
+    var mailOptions = {
+      from: 'timtudosa6@gmail.com',
+      to: 'timtudosa6@gmail.com',
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+  });
+}
 
   catch(e) {
     console.log(e);
   }
 });
-
